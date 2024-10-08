a0898ab2df751e900ec85031c79726dca8f0c2ce
barcelos-kevin <barceloskevinjoseph@gmail.com>
2024-10-08 10:08:00 +0800

    Reset

--- consultado_kirby.py
+++ consultado_kirby.py
@@ -2,5 +2,3 @@ print("Name: Kirby G. Consultado")
 print("Birthday: September 9, 2005")
 print("Gender: Male")
 print("Email: kirbyconsultado@gmail.com")
-
-print ("Hi kirby I'm your current teammate - Edriane")
