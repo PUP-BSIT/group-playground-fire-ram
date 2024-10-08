1d9698dab4db00303337ca73cc8a9ca2d9668425
barcelos-kevin <barceloskevinjoseph@gmail.com>
2024-10-08 09:33:14 +0800

    Say personal message

--- andaya_gener.py
+++ andaya_gener.py
@@ -2,3 +2,5 @@ print("Name: Gener A. Andaya jr.")
 print("Birthday: September 17, 2004")
 print("Gender: Male")
 print("Email: generandaya4@gmail.com")
+
+print("Hello Gener welcome to the team -kevin")
