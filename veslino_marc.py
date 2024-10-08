f59bdf8953fb16e063774b8485c498c7ba6d364b
consultado <kirbyconsultado@gmail.com>
2024-10-08 10:40:25 +0800

    add personal message - Kirby

--- veslino_marc.py
+++ veslino_marc.py
@@ -2,3 +2,5 @@ print ("Name: Marc D. Veslino")
 print ("Birthday: May 4, 2005")
 print ("Gender: Male")
 print ("Email: marcveslino000@gmail.com")
+
+print("Hi Gener, I am glad to work with you. - Kirby")
