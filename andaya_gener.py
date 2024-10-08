d7c6dfd1b164c1f4daf28440fbd7356a4731bd25
piadozo-edriane <doidoiwey@gmail.com>
2024-10-08 09:37:52 +0800

    Add changes to the file

--- andaya_gener.py
+++ andaya_gener.py
@@ -3,6 +3,4 @@ print("Birthday: September 17, 2004")
 print("Gender: Male")
 print("Email: generandaya4@gmail.com")
 
-print("Hello Gener welcome to the team -kevin")
-
-print ("Hi Gener this your teammate -Edriane ")
+print ("Hello Gener your teammate  - Edriane")
