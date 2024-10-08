83bbac7d94985a9bee03aa679d2591e4c886695c
piadozo-edriane <doidoiwey@gmail.com>
2024-10-08 09:42:43 +0800

    add changes

--- barcelos_kevin.py
+++ barcelos_kevin.py
@@ -2,3 +2,5 @@ print("Name: Kevin Joseph V. Barcelos")
 print("Birthday: February 18, 2005")
 print("Gender: Male")
 print("Email: barceloskevinjoseph2gmail.com")
+
+print ("Hi kevin I'm your teammate -Edriane")
