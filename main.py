4c828f32a01ffa39302f3dd95ec93c72dd1a9f2d
consultado <kirbyconsultado@gmail.com>
2024-10-21 20:22:31 +0800

    Instruction and assignents, currently working. - Kirby

--- main.py
+++ main.py
@@ -7,13 +7,29 @@ a. Product Name
 b. Price
 c. Quantity
 
-After the last detail, it will ask if the user wants to add another item answerable by y for yes or n for no. If the user enters y, it will ask order details. Otherwise, it will proceed to the next step. After the orders are completed, the program will ask for the customer name and senior ID no.(blank if not senior citizen). If the customer is a senior citizen, a 10% discount will be deducted in the grand total. After that, the program will display the the following details:
+After the last detail, it will ask if the user wants to add another item answerable by y for
+yes or n for no. If the user enters y, it will ask order details. Otherwise, it will proceed
+to the next step. After the orders are completed, the program will ask for the customer name
+and senior ID no.(blank if not senior citizen). If the customer is a senior citizen, a 10%
+discount will be deducted in the grand total. After that, the program will display the the
+following details:
 
 d. Items(product name, price, quantity, total)
 e. Customer Name
 f. Senior ID No.
 g. Grand Total
 
+# Member 1: Function to gather order details.
+# Member 2: Function to get customer details.
+# Member 3: Function to calculate the total of the orders.
+# Member 4: Function to apply senior discount.
+# Member 5: Function to display order summary.
+
+Member 1: @Marc Veslino
+Member 2: @Kirby Consultado
+Member 3: @Piadozo Edriane
+Member 4: @Kevin Barcelos
+Member 5: @Jay-ar Andaya
 
 """
 list_of_product = ["Nescafe", "Rebisco", "Lucky Me"]
@@ -124,3 +140,8 @@ match choice:
                 print ()
     case _:
         print ("Invalid choice")
+
+"""
+Kirby is working.
+
+"""
