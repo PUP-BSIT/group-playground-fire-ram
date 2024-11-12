530bee0ef26633b14e3773db12bc80ef1584b25c
veslino-marc <veslinomarc4@gmail.com>
2024-11-12 20:47:42 +0800

    Update initial file

--- practice_exe9.py
+++ practice_exe9.py
@@ -6,7 +6,7 @@ data_record = {
     "Role": [],
     "Major": [],
     "Average": [],
-}
+} 
 
 def list_of_dict():
     for key in data_record:
@@ -36,10 +36,11 @@ def add_in_dict(range_of_dict):
     return " "
 
 def update_in_dict():
-    new_info = input("Enter the list (Name, Age, Role, Major, Average) you want to update: ")
+    print("Follow the format (Name, Age, Role, Major, Average) ")
+    new_info = input("Enter the list you want to update: ")
     if new_info in data_record:
-        key_info = int(input("Enter the key index you want to update: "))
-        new_value = input (f"Enter the new value for {new_info}: ")
+        key_info = int(input(f"Enter the key index you want to update (0 - {len(data_record[new_info]) - 1}): "))
+        new_value = input(f"Enter the new value for {new_info}: ")
         data_record[new_info][key_info] = new_value
     else:
         print("No data record in dictionary")
@@ -47,12 +48,13 @@ def update_in_dict():
     return " "
 
 def delete_in_dict():
-    list_info = input("Enter the information you want to delete in this records (Name, Age, Role, Major, Average): ")
+    list_info = input("Enter the information you want to delete in this dictionary (Name, Age, Role, Major, Average): ")
 
     if list_info in data_record:
         remove_data = input("Enter the data you want to remove: ")
         print("The data is removed.")
         data_record[list_info].remove(remove_data)
+        
         if data_record[list_info] in data_record:
             print("The data is removed. ")
         else:
@@ -64,31 +66,31 @@ def delete_in_dict():
 
 def search_of_dict(search_list):
     if search_list in data_record:
-        print("List is on our record")
-        print(f"Do you want to continue searching on {search_list}: ")
-        choice = input("Print Y or y to continue X or x to stop: ")
+        print("The list is on dictionary.")
+        print(f"Do you want to continue searching on {search_list} ? ")
+        choice = input("Type 'Y' or 'y' to continue, 'X' or 'x' to stop: ")
 
         if choice == "Y" or choice == "y":
-            key_finder = int (input("Enter the key index you want to find starting from 0: "))
+            key_finder = int(input(f"Enter the key index you want to find (0 - {len(data_record[search_list]) - 1}): "))
             data_record[search_list][key_finder]
             value = data_record[search_list][key_finder]
-            print(f"The index that you have enter is: {value}")
+            print(f"The index that you have entered is {value}")
         else:
             exit       
     else:
-        print("List is not on record")
+        print("The list is not on dictionary.")
 
-        return " "
+    return " "
 
 while True:
-    os.system ('cls')
-    print ("Student Information Records")
-    print ("1. List all")
-    print ("2. Add")
-    print ("3. Update")
-    print ("4. Delete")
-    print ("5. Search")
-    print ("6. Exit")
+    os.system('cls')
+    print("Student Information Records")
+    print("1. List All")
+    print("2. Add")
+    print("3. Update")
+    print("4. Delete")
+    print("5. Search")
+    print("6. Exit")
 
     choice = input("Enter your choice in the menu (1-6): ")
 
@@ -97,30 +99,33 @@ while True:
 
     match choice:
         case "1":
-            os.system ('cls')
-            print (list_of_dict())
+            os.system('cls')
+            print(list_of_dict())
             input("Press enter to continue")
 
         case "2":
             os.system('cls')
             range_of_dictionary = int(input("How many student(s) information you want to add: "))
-            add_in_dict (range_of_dictionary)
+            add_in_dict(range_of_dictionary)
             input("Press enter to continue")
             
         case "3":
-            os.system ('cls')
-            print (update_in_dict())
+            os.system('cls')
+            print(update_in_dict())
             input("Press enter to continue")
+        
         case "4":
-            os.system ('cls')
-            print (delete_in_dict())
+            os.system('cls')
+            print(delete_in_dict())
             input("Press enter to continue")
 
         case "5":
             os.system('cls')
-            search_input = input("Enter the list you want to search Enter the list (Name, Age, Role, Major, Average): ")
-            print (search_of_dict(search_input))
+            print("Follow the format (Name, Age, Role, Major, Average) ")
+            search_input = input("Enter the list you want to search in the dictionary: ")
+            print(search_of_dict(search_input))
             input("Press enter to continue")
+        
         case _:
-            os.system ('cls')
-            print ("Invalid choice")
+            os.system('cls')
+            print("Invalid choice")
