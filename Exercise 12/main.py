2120f1ada2db59add828fa854c3a4e05b7af29a6
barcelos-kevin <barceloskevinjoseph@gmail.com>
2024-12-08 17:45:12 +0800

    Add barcelos branch and module

--- Exercise 12/main.py
+++ Exercise 12/main.py
@@ -1,15 +1,18 @@
 from ourpackages import barcelos
+import os
 
+os.system('cls')
 print("(1) Kevin Joseph V. Barcelos")
 print("(2) Marc D. Veslino")
 print("(3) Kirby G. Consultado")
 print("(4) Gener A. Anadaya")
 print("(5) Edriane O. Piadozo")
-user_choice = int(input("Enter your choice"))
+user_choice = int(input("Enter your choice: "))
 
 match(user_choice):
     case 1:
-        pass
+        os.system('cls')
+        barcelos.kevin()
     case 2:
         pass
     case 3:
---
+++ Exercise 12/ourpackages/barcelos.py
@@ -0,0 +1,22 @@
+def kevin():
+    print("Hello everyone! I am Kevin Joseph V. Barcelos")
+    print("1. Basic Information")
+    print("2. Goals")
+    print("3. Comment Teammate-1")
+    print("4. Comment Teammate-2")
+    print("5. Comment Teammate-3")
+    print("6. Comment Teammate-4")
+    menu_choice = int(input("Enter your choice: "))
+    match(menu_choice):
+        case 1:
+            pass
+        case 2:
+            pass
+        case 3:
+            pass
+        case 4:
+            pass
+        case 5:
+            pass
+        case 6:
+            pass
