927db1e00875af1419b2293a80f44cc8b073858f
veslino-marc <veslinomarc4@gmail.com>
2024-12-08 20:52:04 +0800

    Add my module - Marc

--- Exercise 12/main.py
+++ Exercise 12/main.py
@@ -5,13 +5,13 @@ print("(2) Marc D. Veslino")
 print("(3) Kirby G. Consultado")
 print("(4) Gener A. Anadaya")
 print("(5) Edriane O. Piadozo")
-user_choice = int(input("Enter your choice"))
+user_choice = int(input("Enter your choice: "))
 
-match(user_choice):
+match user_choice:
     case 1:
         pass
     case 2:
-        pass
+        veslino.marc_information()
     case 3:
         pass
     case 4:
