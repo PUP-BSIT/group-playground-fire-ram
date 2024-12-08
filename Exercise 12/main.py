739aa2b90eab9cf5bffb5e37bf474bb51213169d
barcelos-kevin <barceloskevinjoseph@gmail.com>
2024-12-08 17:36:38 +0800

    add menu in main.py

---
+++ Exercise 12/main.py
@@ -0,0 +1,19 @@
+from ourpackages import barcelos
+
+print("(1) Kevin Joseph V. Barcelos")
+print("(2) Marc D. Veslino")
+print("(3) Kirby G. Consultado")
+print("(4) Gener A. Anadaya")
+print("(5) Edriane O. Piadozo")
+user_choice = int(input("Enter your choice"))
+
+match(user_choice):
+    case 1:
+        pass
+    case 2:
+        pass
+    case 3:
+        pass
+    case 4:
+        pass
+    
