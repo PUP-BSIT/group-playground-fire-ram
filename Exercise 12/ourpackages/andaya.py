afb7cc71759ee4c816fe6d1d231a218360933165
andaya-gener <generandaya4@gmail.com>
2024-12-09 11:12:43 +0800

    add changes

--- Exercise 12/ourpackages/andaya.py
+++ Exercise 12/ourpackages/andaya.py
@@ -1,7 +1,9 @@
+import os 
+
 def gener():
 
     while True:
-        print("\n")
+        os.system('cls')
         print("================= Andaya's Menu ==================")
         print("=                                                =")
         print("= Hello Everyone! My name is Gener A. Andaya Jr. =")
@@ -19,29 +21,45 @@ def gener():
        
         match choice:
             case "1":
-                print("\nName: Gener A. Andaya Jr.")
-                print("Age: 20 years old.") 
-                print("Sex: Male.")
-                print("Birthday: September 17, 2004.")
-                print("Civil Status: Single.")
-                print("Religion: Baptist.")
-                print("Course: Diploma in Information Technology (DIT).")
+                print("\n")
+                print("================== My Basic Info ===================")
+                print("=                                                  =")
+                print("= Name: Gener A. Andaya Jr.                        =")
+                print("= Age: 20 years old.                               =") 
+                print("= Sex: Male.                                       =")
+                print("= Birthday: September 17, 2004.                    =")
+                print("= Civil Status: Single.                            =")
+                print("= Religion: Baptist.                               =")
+                print("= Course: Diploma in Information Technology (DIT). =")
+                print("=                                                  =")
+                print("====================================================")
+                input("\nPlease press enter key to continue!")
             case "2":
-             print("\nMy Personal Goals:")
-             print("- Finish my studies in College.")
-             print("- Secure a stable job in the IT field.")
-             print("- Own a house and cars.")
-             print("- Establish my own business.")
-            case "3":
+                print("\n")
+                print("=========== My Personal Goals ============")
+                print("=                                        =")
+                print("= - Finish my studies in College.        =")
+                print("= - Secure a stable job in the IT field. =")
+                print("= - Own a house and cars.                =")
+                print("= - Establish my own business.           =")
+                print("=                                        =")
+                print("==========================================")
+                input("\nPlease press enter key to continue!")
+            case "3":   
                 print("\nTeammate 1 Barcelos: ")
+                input("\nPlease press enter key to continue!")
             case "4":
                 print("\nTeammate 2 Veslino: ")
+                input("\nPlease press enter key to continue!")
             case "5":
-             print("\nTeammate 3 Consultado: ")
+                print("\nTeammate 3 Consultado: ")
+                input("\nPlease press enter key to continue!")
             case "6":
-             print("\nTeammate 4 Piadozo: ")
-            case "7":
-             print ("Exiting Menu. Goodbye!\n")
-             break
+                print("\nTeammate 4 Piadozo: ")
+                input("\nPlease press enter key to continue!")
+            case "7":   
+                print ("\nExiting Menu. Goodbye!\n")
+                break
             case _:
-             print("\nInvalid choice. Please try again.")
+                print("\nInvalid choice. Please try again.")
+                input("\nPlease press enter key to continue!")
